e229bcf198820262a8597f3adf552b5159931c29
BogdusUltra <bogdus712@gmail.com>
2025-12-18 14:59:43 +0300

    Final Commit

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -6,6 +6,15 @@ namespace Lab6
     {
         public static void Main()
         {
+            Purple purple = new Purple();
+
+            int[] arr = new int[6] { 1, 0, -4, 4, -1, -10 };
+            purple.SortNegativeDescending(arr);
+            Console.WriteLine(string.Join(" ", arr));
+            
+             arr = new int[6] { 1, 0, -4, 4, -1, -10 };
+            purple.SortNegativeAscending(arr);
+            Console.WriteLine(string.Join(" ", arr));
         }
     }
 }
